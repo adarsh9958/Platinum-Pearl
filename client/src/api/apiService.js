ab6699513eb59f7c6bad8c53648ff07f8cf14f34
Adarsh Pathak <pathakadarsh720@gmail.com>
2025-08-14 05:26:16 +0530

    feat: Update API URL for deployment

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // The base URL for your backend server
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://platinum-pearl.onrender.com/';
 
 const apiService = axios.create({
   baseURL: API_URL,
